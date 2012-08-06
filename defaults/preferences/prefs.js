d67a6d994330c3c16aae03c047d305eddba65590
MarkaPola <markapola@git.dev>
2012-08-06 14:37:35 +0200

    Sync implementation

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -6,6 +6,7 @@
 /* Linkificator preferences - Linkificator's module
  * author: MarkaPola */
 
+pref("extensions.linkificator@markapola.sync", false);
 pref("extensions.linkificator@markapola.activated", true);
 pref("extensions.linkificator@markapola.hotkey", "control-shift-y");
 pref("extensions.linkificator@markapola.displayWidget", true);
@@ -21,3 +22,18 @@ pref("extensions.linkificator@markapola.backgroundColor", "#fff9ab");
 pref("extensions.linkificator@markapola.protocols", "h..p~http#2;h..ps~https#2;ftp~ftp#2;news~news#0;nntp~nntp#2;telnet~telnet#2;irc~irc#2;file~file#3;about~about#0");
 pref("extensions.linkificator@markapola.subdomains", "www~www\\d{0,3}~http://;ftp~ftp~ftp://;irc~irc~irc://");
 pref("extensions.linkificator@markapola.excludedElements", "a;applet;area;embed;frame;frameset;head;iframe;img;map;meta;noscript;object;option;param;script;select;style;textarea;title;@onclick;@onmousedown;@onmouseup");
+// sync management, deactivated by default
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.hotkey", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.displayWidget", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.useRegExp", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.useWhitelist", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.whitelist", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.useBlacklist", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.blacklist", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.overrideTextColor", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.linkColor", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.overrideBackgroundColor", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.backgroundColor", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.protocols", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.subdomains", false);
+pref("services.sync.prefs.sync.extensions.linkificator@markapola.excludedElements", false);
