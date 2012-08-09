249aec5588fac81468e6630f061d6bec731ae626
MarkaPola <markapola@git.dev>
2012-08-09 15:35:24 +0200

    update UI

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -6,7 +6,6 @@
 /* Linkificator preferences - Linkificator's module
  * author: MarkaPola */
 
-pref("extensions.linkificator@markapola.sync", false);
 pref("extensions.linkificator@markapola.activated", true);
 pref("extensions.linkificator@markapola.hotkey", "control-shift-y");
 pref("extensions.linkificator@markapola.displayWidget", true);
@@ -22,6 +21,7 @@ pref("extensions.linkificator@markapola.backgroundColor", "#fff9ab");
 pref("extensions.linkificator@markapola.protocols", "h..p~http#2;h..ps~https#2;ftp~ftp#2;news~news#0;nntp~nntp#2;telnet~telnet#2;irc~irc#2;file~file#3;about~about#0");
 pref("extensions.linkificator@markapola.subdomains", "www~www\\d{0,3}~http://;ftp~ftp~ftp://;irc~irc~irc://");
 pref("extensions.linkificator@markapola.excludedElements", "a;applet;area;embed;frame;frameset;head;iframe;img;map;meta;noscript;object;option;param;script;select;style;textarea;title;@onclick;@onmousedown;@onmouseup");
+pref("extensions.linkificator@markapola.sync", false);
 // sync management, deactivated by default
 pref("services.sync.prefs.sync.extensions.linkificator@markapola.hotkey", false);
 pref("services.sync.prefs.sync.extensions.linkificator@markapola.displayWidget", false);
