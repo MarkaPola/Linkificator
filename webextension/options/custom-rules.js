5bc2a8d8f64a9ba53abdcc1ec374720c71318f0a
MarkaPola <markapola@git.dev>
2017-09-08 17:08:55 +0200

    drap n drop implementation

--- webextension/options/custom-rules.js
+++ webextension/options/custom-rules.js
@@ -130,10 +130,35 @@ class RuleEditor {
 var ruleEditor = new RuleEditor({});
 
 class CustomRule {
+    _editRule (event) {
+        let rule = this._node.querySelector('.rule-name');
+            
+        function updateRule (newRule) {
+            rule.textContent = newRule.name;
+            rule.dataset.pattern = newRule.pattern;
+            rule.dataset.url = newRule.url;
+
+            if (this._options.onEdit) this._options.onEdit(this);
+        }
+            
+        ruleEditor.editRule({rule: {name: rule.textContent,
+                                    pattern: rule.dataset.pattern,
+                                    url: rule.dataset.url},
+                             onOK: updateRule.bind(this)
+                            });
+    }
+    _deleteRule (event) {
+        this.release();
+        
+        if (this._options.onDelete) this._options.onDelete(this);
+    }
+    
     constructor (options) {
-        let {rule, onEdit, onDelete} = options;
+        this._options = options;
         
         // create DOM object tree
+        let rule = options.rule;
+        
         this._node = document.importNode(document.getElementById('custom-rule-template').content, true).querySelector('.settings-rule');
         this._node.querySelector('.rule-active').checked = rule.active;
         let ruleName = this._node.querySelector('.rule-name');
@@ -142,74 +167,73 @@ class CustomRule {
         ruleName.dataset.url = rule.url;
 
         // attach actions for edition/deletion
-        this._node.querySelector('.edit-button').addEventListener('click', event => {
-            let rule = event.target.parentNode.parentNode.querySelector('.rule-name');
-            
-            function updateRule (event, newRule) {
-                rule.textContent = newRule.name;
-                rule.dataset.pattern = newRule.pattern;
-                rule.dataset.url = newRule.url;
-
-                if (onEdit) onEdit(event.target.parentNode);
-            }
-            
-            ruleEditor.editRule({rule: {name: rule.textContent,
-                                        pattern: rule.dataset.pattern,
-                                        url: rule.dataset.url},
-                                 onOK: updateRule.bind(this)
-                                });
-        });
-
-        function deleteNode (event) {
-            if (onDelete) onDelete(event.target.parentNode.parentNode);
-        }
-        this._node.querySelector('.delete-button').addEventListener('click', deleteNode.bind(this));
+        this._node.querySelector('.edit-button').addEventListener('click', this._editRule.bind(this));
+        this._node.querySelector('.delete-button').addEventListener('click', this._deleteRule.bind(this));
     }
 
+    update (options) {
+        this._options = options;
+        
+        let rule = options.rule;
+        
+        this._node.querySelector('.rule-active').checked = rule.active;
+        let ruleName = this._node.querySelector('.rule-name');
+        ruleName.textContent = rule.name;
+        ruleName.dataset.pattern = rule.pattern;
+        ruleName.dataset.url = rule.url;
+    }
+    
     get node () {
         return this._node;
     }
+
+    release () {
+        this._node.querySelector('.edit-button').removeEventListener('click,', this._editRule);
+        this._node.querySelector('.delete-button').removeEventListener('click', this._deleteRule);
+    }
 }
+
 
 class RulesManager {
     _dragStart (event) {
         console.log('DRAGSTART');
-        event.target.style.opacity = '0.4';
+        event.currentTarget.style.opacity = '0.4';
         
-        event.dataTransfer.setData('text', event.target.parentNode.parentNode.rowIndex.toString());
+        event.dataTransfer.setData('text', event.currentTarget.rowIndex.toString());
         event.dataTransfer.effectAllowed = 'move';
     }
     _dragEnter (event) {
         console.log('DRAGENTER');
-        // if (event.target.parentNode.parentNode.rowIndex != event.dataTransfer.getData('Text')) {
-            this.classList.add('dragover');
-        // }
+        if (event.currentTarget.rowIndex != event.dataTransfer.getData('text')) {
+            event.currentTarget.querySelector('.settings-rule').classList.add('dragover');
+        }
         event.preventDefault();
     }
     _dragLeave (event) {
         console.log('DRAGLEAVE');
-        event = event.originalEvent || event;
-        var currentElement = document.elementFromPoint(event.pageX, event.pageY);
-        if (!this.contains(currentElement)) {
-			this.classList.remove('dragover');
+        let currentElement = document.elementFromPoint(event.pageX, event.pageY);
+        if (!event.currentTarget.contains(currentElement)) {
+			event.currentTarget.querySelector('.settings-rule').classList.remove('dragover');
         }
     }
     _dragOver (event) {
         console.log('DRAGOVER');
-        event.preventDefault(); // Necessary. Allows us to drop.
+        if (event.currentTarget.rowIndex != event.dataTransfer.getData('text')) {
+            event.preventDefault(); // Necessary. Allows us to drop.
         
-        event.dataTransfer.dropEffect = 'move';
-
+            event.dataTransfer.dropEffect = 'move';
+        }
+        else
+            event.dataTransfer.dropEffect = 'none';
+        
         return false;
     }
     _dragEnd (event) {
         console.log('DRAGEND');
-        // target is the source node
-        event.target.style.opacity = '1.0';
-        
-        let nodes = this._table.querySelectorAll('.settings-rule');
-        for (let node of nodes) {
-            node.classList.remove('dragover');
+
+        for (let row of this._table.rows) {
+            row.style.opacity = '';
+            row.querySelector('.settings-rule').classList.remove('dragover');
         }
     }
     _drop (event) {
@@ -220,24 +244,14 @@ class RulesManager {
         return false;
     }
     
-    _newRule (options) {
-        let rule = new CustomRule(options).node;
-
-        // Drag n drop management
-        rule.addEventListener('dragstart', this._dragStart, false);
-        rule.addEventListener('dragenter', this._dragEnter, false);
-        rule.addEventListener('dragover', this._dragOver, false);
-        rule.addEventListener('dragleave', this._dragLeave, false);
-        rule.addEventListener('dragend', this._dragEnd.bind(this), false);
-        rule.addEventListener('drop', this._drop, false);
-
-        return rule;
-    }
-    _editRow (node) {
+    _ruleEdited (rule) {
         if (this._onChange) this._onChange();
     }
-    _deleteRow (node) {
-        this._table.deleteRow(node.parentNode.parentNode.rowIndex);
+    _ruleDeleted (rule) {
+        // look-up row from CustomRule
+        let entries = Array.from(this._rows.entries()).filter(entry => entry[1] === rule);
+        
+        this.deleteRow(entries[0][0].rowIndex);
 
         if (this._onChange) this._onChange();
     }
@@ -245,6 +259,8 @@ class RulesManager {
     constructor (options) {
         let {id, rules, display} = options;
 
+        this._rows = new Map();
+        
         this._table = $(id);
         display ? this.show() : this.hide();
 
@@ -252,39 +268,102 @@ class RulesManager {
 
         if (rules) {
             for (let rule of rules) {
-                let r = this._newRule({rule: rule,
-                                       onEdit: this._editRow.bind(this),
-                                       onDelete: this._deleteRow.bind(this)});
-                                      
-                this._table.insertRow(-1).insertCell(0).appendChild(r);
-
+                this.insertRow(-1, {rule: rule,
+                                    onEdit: this._ruleEdited.bind(this),
+                                    onDelete: this._ruleDeleted.bind(this)});
             }
         }
 
         // handle drag n' drop on rules overflow area
+        function dragOverOverflow (event) {
+            console.log('DRAGOVER REMAIN');
+            if (event.dataTransfer.getData('text') < (this._table.rows.length-1)) {
+                event.preventDefault(); // Necessary. Allows us to drop.
+        
+                event.dataTransfer.dropEffect = 'move';
+            }
+            else
+                event.dataTransfer.dropEffect = 'none';
+        }
         let rulesArea = $('rules-overflow');
         rulesArea.addEventListener('dragenter', event => {
             console.log('DRAGENTER REMAIN');
             event.preventDefault();
         }, false);
-        rulesArea.addEventListener('dragover', event => {
-            console.log('DRAGOVER REMAIN');
-            event.preventDefault();
-        
-            event.dataTransfer.dropEffect = 'move';
-        }, false);
+        rulesArea.addEventListener('dragover', dragOverOverflow.bind(this));
         rulesArea.addEventListener('drop', event => {
             console.log('DROP REMAIN');
             event.stopPropagation(); // stops the browser from redirecting.
             event.preventDefault();
         }, false);
     }
-    
-    show () {
-        this._table.classList.remove('hidden');
+
+    insertRow (index, options) {
+        let rule = new CustomRule(options);
+        let row = this._table.insertRow(index);
+
+        this._rows.set(row, rule);
+        
+        // Drag n drop management
+        row.addEventListener('dragstart', this._dragStart.bind(this));
+        row.addEventListener('dragenter', this._dragEnter.bind(this));
+        row.addEventListener('dragover', this._dragOver.bind(this));
+        row.addEventListener('dragleave', this._dragLeave.bind(this));
+        row.addEventListener('dragend', this._dragEnd.bind(this));
+        row.addEventListener('drop', this._drop.bind(this));
+        
+        row.insertCell(0).appendChild(rule.node);
+
+        return row;
     }
-    hide () {
-        this._table.classList.add('hidden');
+
+    updateRow (index, options) {
+        let row = this._table.rows[index];
+        let rule = this._rows.get(row);
+
+        rule.update(options);
+
+        return row;
+    }
+
+    moveRow (source,  target) {
+        let sourceRow = this._table.rows[source];
+        let rule = this._rows.get(sourceRow);
+
+        this._table.deleteRow(source);
+        this._rows.delete(sourceRow);
+
+        let targetRow = this._table.insertRow(target);
+        this._rows.set(targetRow, rule);
+
+        targetRow.addEventListener('dragstart', this._dragStart.bind(this));
+        targetRow.addEventListener('dragenter', this._dragEnter.bind(this));
+        targetRow.addEventListener('dragover', this._dragOver.bind(this));
+        targetRow.addEventListener('dragleave', this._dragLeave.bind(this));
+        targetRow.addEventListener('dragend', this._dragEnd.bind(this));
+        targetRow.addEventListener('drop', this._drop.bind(this));
+
+        targetRow.insertCell(0).appendChild(rule.node);
+    }
+    
+    deleteRow (index) {
+        let row = this._table.rows[index];
+        
+        // remove handlers
+        row.removeEventListener('dragstart', this._dragStart.bind(this));
+        row.removeEventListener('dragenter', this._dragEnter.bind(this));
+        row.removeEventListener('dragover', this._dragOver.bind(this));
+        row.removeEventListener('dragleave', this._dragLeave.bind(this));
+        row.removeEventListener('dragend', this._dragEnd.bind(this));
+        row.removeEventListener('drop', this._drop.bind(this));
+
+        let rule = this._rows.get(row);
+        rule.release();
+        this._rows.delete(row);
+        
+        this._table.deleteRow(index);
+
+        if (this._onChange) this._onChange();
     }
 
     update (rules) {
@@ -292,33 +371,30 @@ class RulesManager {
         let index = 0;
         
         for (let rule of rules) {
-            let r = this._newRule({rule: rule,
-                                   onEdit: this._editRow.bind(this),
-                                   onDelete: this._deleteRow.bind(this)});
-            
             if (index < size) {
-                // update current row
-                let cell = this._table.rows[index].cells[0];
-                cell.replaceChild(r, cell.querySelector('.settings-rule'));
+                this.updateRow(index, {rule: rule,
+                                       onEdit: this._ruleEdited.bind(this),
+                                       onDelete: this._ruleDeleted.bind(this)});
                 index++;
             } else {
-                this._table.insertRow(-1).insertCell(0).appendChild(r);
+                this.insertRow(-1, {rule: rule,
+                                    onEdit: this._ruleEdited.bind(this),
+                                    onDelete: this._ruleDeleted.bind(this)});
             }
         }
 
         // remove obsolete rows
         for (let i = index; index < size; i++) {
-            this._table.deleteRow(i);
+            this.deleteRow(i);
         }
     }
     
     addRule () {
         function appendRule (event, rule) {
             rule.active = true;
-            let r = new CustomRule({rule: rule,
-                                    onEdit: this._editRow.bind(this), 
-                                    onDelete: this._deleteRow.bind(this)});
-            this._table.insertRow(-1).insertCell(0).appendChild(r.node);
+            this.insertRow(-1, {rule: rule,
+                                onEdit: this._ruleEdited.bind(this), 
+                                onDelete: this._ruleDeleted.bind(this)});
 
             if (this._onChange) this._onChange();
         }
@@ -341,5 +417,12 @@ class RulesManager {
         }
         
         return result;
+    }
+    
+    show () {
+        this._table.classList.remove('hidden');
+    }
+    hide () {
+        this._table.classList.add('hidden');
     }
 }
